31ed97ea41dac5b4e0580b7b36c36cd53ff9ffc0
Joe <67245465+joe-ldp@users.noreply.github.com>
2022-05-30 11:23:02 +0100

    new & improved world bopper
    leaves most recent 5 worlds untouched

--- scripts/worldBopper9000.py
+++ scripts/worldBopper9000.py
@@ -1,15 +1,22 @@
 # v1.0.0-beta
 
-import glob
-import sys
-import shutil
+import glob, sys, shutil, os
+from operator import itemgetter
 
-files = glob.glob(sys.argv[1] + '/saves/*')
-print(files)
-for f in files:
-    try:
-        if "New World" in f or "Speedrun #" in f:
-            shutil.rmtree(f)
-    except:
-        # well fuck
-        b = 7 # only real ones know
+def sort_files_by_last_modified(files):
+    file_data = {}
+    for fname in files:
+        if "New World" in fname or "Speedrun #" in fname:
+            file_data[fname] = os.stat(fname).st_mtime
+
+    file_data = sorted(file_data.items(), key = itemgetter(1))
+    return file_data
+
+def delete_oldest_files(sorted_files):
+    for x in range(0, len(sorted_files) - 5):
+        shutil.rmtree(sorted_files[x][0])
+
+file_paths = glob.glob(sys.argv[1] + "/saves/*")
+sorted_files = sort_files_by_last_modified(file_paths)
+
+delete_oldest_files(sorted_files)
