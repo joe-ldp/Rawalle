a9401796e8a6938178fb73cdd175b78fac6fe761
Joe <67245465+joe-ldp@users.noreply.github.com>
2022-07-02 04:02:38 +0100

    leave past 10 worlds on worldBopper
    just a bit safer

--- scripts/worldBopper9000.py
+++ scripts/worldBopper9000.py
@@ -15,7 +15,7 @@ def sort_files_by_last_modified(files):
     return file_data
 
 def delete_oldest_files(sorted_files):
-    for x in range(0, len(sorted_files) - 5):
+    for x in range(0, len(sorted_files) - 10):
         shutil.rmtree(sorted_files[x][0])
 
 file_paths = glob.glob(sys.argv[1] + "/saves/*")
