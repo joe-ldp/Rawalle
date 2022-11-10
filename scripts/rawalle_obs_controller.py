c62d7b07fa743e57a11cfe7d142b21f78aa1b7de
Joe <joedkirkup@gmail.com>
2022-11-10 00:11:48 +0000

    use recursive find for grouped locks

--- scripts/rawalle_obs_controller.py
+++ scripts/rawalle_obs_controller.py
@@ -60,7 +60,7 @@ def execute_cmd(cmd):
             lock_num = cmd[1]
             render = True if int(cmd[2]) else False
             wall_scene = S.obs_scene_get_source(S.obs_get_scene_by_name(settings.wall_scene))
-            lock_source = S.obs_scene_find_source(S.obs_scene_from_source(wall_scene), settings.lock_layer_format.replace("*", str(lock_num)))
+            lock_source = S.obs_scene_find_source_recursive(S.obs_scene_from_source(wall_scene), settings.lock_layer_format.replace("*", str(lock_num)))
             S.obs_sceneitem_set_visible(lock_source, render)
         elif (cmd[0] == "Reload"):
             script_init()
